e463c6e24f448544fb95ef60095605f6194378f7
myname21-meet <myname21@meet.mit.edu>
2019-07-24 07:15:52 -0400

    jquerylab

--- style/style.js
+++ style/style.js
@@ -3,16 +3,19 @@
 
 
 function updateSize(newSize) {
+	$("img").css("width", newSize)
   // FINISH ME! Set the width of the MEET logo to be newSize in pixels.
 }
 
 
 function updateLeftPosition(newX) {
+	$("img").css("transform", "rotateX("+ newX+ "deg)");
 	// FINISH ME
 
 }
 
 function updateTopPosition(newY) {
+	$("img").css("transform", "rotateY("+ newY+ "deg)");
 	// FINISH ME
 }
 
